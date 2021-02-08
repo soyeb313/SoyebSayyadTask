0846f885d7679cc6e1dd01545ee89ba83020a795
Ajay M <ajaym@techved.com>
2021-02-08 11:22:39 +0530

    Initial Commit

---
+++ SoyebSayyadTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoyebSayyadTask
+//
+//  Created by Mac on 08/02/21.
+//  Copyright © 2021 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
